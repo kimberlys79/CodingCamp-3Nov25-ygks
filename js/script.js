8dddf7a922aaeb2597ea1c54400aad8023ad8d5e
kimberlys79 <ygksianipar@gmail.com>
2025-11-08 06:15:34 +0700

    Add logo and main header styling; enhance HTML structure and content

--- js/script.js
+++ js/script.js
@@ -1,1 +1,16 @@
-console.log('Hello World');
+/// Display a welcome message when the page loads
+welcomeMessage();
+
+function welcomeMessage() {
+    /// Prompt the user for their name
+    let name = prompt("Please enter your name:");
+
+    if (name != null) {
+        
+        /// Set the welcome message with the user's name
+        document.getElementById('welcome-speech').innerHTML = "Welcome, " + name + "! Enjoy exploring our website.";
+    } 
+}
+
+/// Validate the contact form
+function validateForm() { }
